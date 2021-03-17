8be0da687bf8e10eecf1b9634aab8e22679b23ab
Yasmin <yasmin@lit.ifce.edu.br>
2021-03-17 17:30:06 -0300

    ci

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    
+    public function test_example()
+    {
+        $this->get('/')
+             ->assertStatus(200);
+    }
+}
