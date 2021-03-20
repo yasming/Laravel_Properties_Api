d9ba9bac6d211028cfac26993bb088f38645dc15
Yasmin <yasmin@lit.ifce.edu.br>
2021-03-20 12:04:55 -0300

    schedule job to clear and reset cache

---
+++ app/Console/Commands/ResetCacheProperties.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\Properties\PropertiesService;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+
+class ResetCacheProperties extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:reset-cache';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear properties caches and set it again';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        Cache::forget('allProperties');
+        Cache::forget('zapProperties');
+        Cache::forget('vivaRealProperties');
+        $this->info('Cache cleared');
+        (new PropertiesService)->setAllProperties()
+                               ->setZapProperties()
+                               ->setVivaRealProperties();
+        $this->info('Cache setted');
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('command:reset-cache')->daily()->timezone('America/Sao_Paulo')->at('1:00');
     }
 
     /**
