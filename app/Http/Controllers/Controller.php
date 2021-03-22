1d58176a3392c8ba3deb6e9e2ce93ef275ef1cde
yasmin <yasmin@lit.ifce.edu.br>
2021-03-21 21:40:26 -0300

    feature tests

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,11 +9,12 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    
     const ZAP               = 'zap';
     const VIVA_REAL         = 'viva_real';
     const PAGINATION_NUMBER = 10;
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
+    
     public function apiResponse($key,$value)
     {
         return response()->json([$key => $value]);
---
+++ tests/Feature/Zap/ZapControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Zap;
+
+use Tests\TestCase;
+use App\Services\Properties\PropertiesService;
+use App\Http\Controllers\Controller;
+
+class ZapControllerTest extends TestCase
+{
+    private $service;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        ini_set('memory_limit', '2G');
+        $this->service = (new PropertiesService)->setAllProperties()
+                                                ->setZapProperties()
+                                                ->setVivaRealProperties();
+    }
+
+    public function test_it_should_get_zap_properties()
+    {
+        $response = $this->get('/api/zap-properties')
+                         ->assertStatus(200);
+        $this->assertEquals(
+                                $response->decodeResponseJson()['zap'],
+                                $this->service->getZapProperties()->paginate(Controller::PAGINATION_NUMBER)
+                                                                  ->toArray()
+                            ); 
+    }
+}
